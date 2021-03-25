a6a9848b8b2cb0fb2987b9fd73b899bd42f87b7e
John Best <jkbest@gmail.com>
2021-03-25 15:36:26 -0700

    Expand to six OM values; increase q var

--- src/sim_pars.jl
+++ src/sim_pars.jl
@@ -1,4 +1,3 @@
-
 """
     sim_values(::Type{T}) where T<:SpatQSimSpec
     sim_values(::T) where T<:SpatQSimSpec
@@ -6,9 +5,9 @@
 Return the values of the parameters that change for each simulation study.
 """
 sim_values(::T) where T<:SpatQSimSpec = sim_values(T)
-sim_values(::Type{<:QDevScalingSpec}) = 10.0 .^ (-5:-1)
+sim_values(::Type{<:QDevScalingSpec}) = 10.0 .^ (-3:2)
 sim_values(::Type{<:SharedQSpec}) = 0:0.2:1
-sim_values(::Type{<:PrefIntensitySpec}) = [1, 2, 4, 8, 16]
+sim_values(::Type{<:PrefIntensitySpec}) = [0, 1, 2, 4, 8, 16]
 
 function sim_value_idx(spec::SpatQSimSpec)
     findfirst(isapprox(sim_value(spec)),
