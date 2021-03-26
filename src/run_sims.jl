6ecccfc7e1a5e044097e29e85db4c98007a9863f
John Best <jkbest@gmail.com>
2021-03-26 14:22:45 -0700

    Default run_sims to single replicate number

--- src/run_sims.jl
+++ src/run_sims.jl
@@ -12,29 +12,33 @@ function run_sims(simtype::Type{<:SpatQSimSpec},
                   repl_range::AbstractRange{<:Integer};
                   prep_file = "prep.h5",
                   checkpoint = true)
-    study_range = sim_values(simtype)
     for rlz in repl_range
-        for val in study_range
-            spec = simtype(rlz, val, prep_file)
-            files = file_paths(spec)
-
-            # If checkpointing, skip the rest of the inner for loop body if the
-            # results files are present.
-            if (checkpoint && all(isfile.(files)))
-                continue
-            end
-
-            setup = SpatQSimSetup(spec)
-            result = simulate(setup)
-            save(result)
-        end
+        run_sims(simtype,
+                 rlz;
+                 prep_file = prep_file,
+                 checkpoint = checkpoint)
     end
     nothing
 end
 
 function run_sims(simtype::Type{<:SpatQSimSpec},
-                  n_repl::Integer;
+                  rlz::Integer;
                   prep_file = "prep.h5",
                   checkpoint = true)
-    run_sims(simtype, 1:n_repl; prep_file = prep_file, checkpoint = checkpoint)
+    study_range = sim_values(simtype)
+    for val in study_range
+        spec = simtype(rlz, val, prep_file)
+        files = file_paths(spec)
+
+        # If checkpointing, skip the rest of the inner for loop body if the
+        # results files are present.
+        if (checkpoint && all(isfile.(files)))
+            continue
+        end
+
+        setup = SpatQSimSetup(spec)
+        result = simulate(setup)
+        save(result)
+    end
+    nothing
 end
