122442c79ec8487e5e7d6cf5a268a84fb65ec0f2
John Best <jkbest@gmail.com>
2021-03-26 13:36:01 -0700

    Tighten range of qdevscaling pars

--- src/sim_pars.jl
+++ src/sim_pars.jl
@@ -5,7 +5,7 @@
 Return the values of the parameters that change for each simulation study.
 """
 sim_values(::T) where T<:SpatQSimSpec = sim_values(T)
-sim_values(::Type{<:QDevScalingSpec}) = 10.0 .^ (-3:2)
+sim_values(::Type{<:QDevScalingSpec}) = 10.0 .^ (-3:0.5:-0.5)
 sim_values(::Type{<:SharedQSpec}) = 0:0.2:1
 sim_values(::Type{<:PrefIntensitySpec}) = [0, 1, 2, 4, 8, 16]
 
