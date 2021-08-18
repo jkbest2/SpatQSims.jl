d8eb9180c40288fba9ee01658ed884b84dcb4fdd
John Best <jkbest@gmail.com>
2021-08-18 16:06:19 -0700

    Add non-file-based SpatQSimPrep inner constructor

--- src/simprep.jl
+++ src/simprep.jl
@@ -1,24 +1,36 @@
-struct SpatQSimPrep{M, P, Q}
+struct SpatQSimPrep{M, P, Q, F}
+    realization::Int
     movement::M
     init_pop::P
     log_qdevs::Q
-    realization::Int
-    prep_file::String
+    prep_file::F
 
-    function SpatQSimPrep(realization::Int, prep_file::String)
-        _, movement, init_pop, _, log_qdevs =
-            get_realization(realization, prep_file)
-        movement = MovementModel(movement)
-        init_pop = PopState(init_pop)
-        M = typeof(movement)
-        P = typeof(init_pop)
-        Q = typeof(log_qdevs)
-        new{M, P, Q}(movement,
-                     init_pop,
-                     log_qdevs,
-                     realization,
-                     prep_file)
+    function SpatQSimPrep(realization::Int,
+                          movement::M,
+                          init_pop::P,
+                          log_qdevs::Q,
+                          prep_file::F = nothing) where {M<:Movement,
+                                                         P<:PopState,
+                                                         Q<:Matrix,
+                                                         F<:Union{String, Nothing}}
+        new{M, P, Q, F}(realization, movement, init_pop, log_qdevs, prep_file)
     end
+end
+
+function SpatQSimPrep(realization::Int, prep_file::String)
+    _, movement, init_pop, _, log_qdevs =
+        get_realization(realization, prep_file)
+    movement = MovementModel(movement)
+    init_pop = PopState(init_pop)
+    M = typeof(movement)
+    P = typeof(init_pop)
+    Q = typeof(log_qdevs)
+    F = typeof(prep_file)
+    new{M, P, Q, F}(realization,
+                    movement,
+                    init_pop,
+                    log_qdevs,
+                    prep_file)
 end
 
 movement(prep::SpatQSimPrep) = prep.movement
