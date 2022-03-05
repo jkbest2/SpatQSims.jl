033f762a132499b226f70ab1607b82dd520ccf0a
John Best <jkbest@gmail.com>
2022-03-04 17:20:03 -0800

    Update SpatQSimSetup constructors & add accessors

--- src/setup.jl
+++ src/setup.jl
@@ -1,85 +1,98 @@
+# Constructors ------------------------------------------------------------------
 """
 A struct to contain all elements required to simulate a fishery.
 """
-struct SpatQSimSetup{P,F,M,R,D,S}
+struct SpatQSimSetup{S,P,F,M,R,D,}
+    spec::S
     init_pop::P
     fleet::F
     movement::M
     pop_dynamics::R
     domain::D
     n_years::Int
-    spec::S
 
-    function SpatQSimSetup(init_pop::P,
+    function SpatQSimSetup(spec::S,
+                           init_pop::P,
                            fleet::F,
                            movement::M,
                            pop_dynamics::R,
                            domain::D,
-                           n_years::Int,
-                           spec::S) where {
+                           n_years::Int) where {
+                               S<:SpatQSimSpec,
                                P<:PopState,
                                F<:Fleet,
                                M<:MovementModel,
                                R<:PopulationDynamicsModel,
-                               D<:AbstractFisheryDomain,
-                               S<:SpatQSimSpec}
-        new{P,F,M,R,D,S}(init_pop, fleet, movement, pop_dynamics, domain, n_years, spec)
+                               D<:AbstractFisheryDomain}
+        new{P,F,M,R,D,S}(spec, init_pop, fleet, movement, pop_dynamics, domain, n_years)
     end
 end
 
 function SpatQSimSetup(prep::SpatQSimPrep)
-    SpatQSimSetup(prep.init_pop,
+    spec = simspec(prep)
+    SpatQSimSetup(spec,
+                  init_pop(prep),
                   fleet(prep),
                   movement(prep),
-                  pop_dynamics(prep.spec),
-                  domain(prep.spec),
-                  SIM_NYEARS,
-                  prep.spec)
+                  pop_dynamics(spec),
+                  domain(spec),
+                  SIM_NYEARS)
 end
 
 function SpatQSimSetup(spec::SpatQSimSpec)
     prep = SpatQSimPrep(spec)
-    SpatQSimSetup(spec, prep)
+    SpatQSimSetup(prep)
 end
 
+# Accessors ---------------------------------------------------------------------
+simspec(setup::SpatQSimSetup) = setup.spec
+init_pop(setup::SpatQSimSetup) = setup.init_pop
+fleet(setup::SpatQSimSetup) = setup.fleet
+movement(setup::SpatQSimSetup) = setup.movement
+pop_dynamics(setup::SpatQSimSetup) = setup.pop_dynamics
+domain(setup::SpatQSimSetup) = setup.domain
+n_years(setup::SpatQSimSetup) = set.n_years
+
+# Add a simulate method to the FisherySim function
 function simulate(setup::SpatQSimSetup)
-    P, C = simulate(setup.init_pop,
-                    setup.fleet,
-                    setup.movement,
-                    setup.pop_dynamics,
-                    setup.domain,
-                    setup.n_years)
-    SpatQSimResult(P, C, setup.spec)
+    P, C = simulate(init_pop(setup),
+                    fleet(setup),
+                    movement(setup),
+                    pop_dynamics(setup),
+                    domain(setup),
+                    n_years(setup))
+
+    SpatQSimResult(P, C, simspec(setup))
 end
 
-function SpatQSimSetup(spec::HabQSpec, prep::HabQPrep)
-    fleet = Fleet([survey_vessel(spec, prep),
-                   comm_vessel(spec, prep)],
-                  [length(survey_targeting(spec)), 40_000],
-                  [1, 2])
+# function SpatQSimSetup(spec::HabQSpec, prep::HabQPrep)
+#     fleet = Fleet([survey_vessel(spec, prep),
+#                    comm_vessel(spec, prep)],
+#                   [length(survey_targeting(spec)), 40_000],
+#                   [1, 2])
 
 
-    SpatQSimSetup(init_pop(prep),
-                  fleet,
-                  movement(prep),
-                  pop_dynamics(spec),
-                  domain(prep),
-                  15,
-                  spec)
-end
+#     SpatQSimSetup(init_pop(prep),
+#                   fleet,
+#                   movement(prep),
+#                   pop_dynamics(spec),
+#                   domain(prep),
+#                   15,
+#                   spec)
+# end
 
-function SpatQSimSetup(spec::BycatchSpec, prep::BycatchPrep)
-    fleet = Fleet([survey_vessel(spec, prep),
-                   comm_vessel(spec, prep)],
-                  [length(survey_targeting(spec)), 40_000],
-                  [1, 2])
+# function SpatQSimSetup(spec::BycatchSpec, prep::BycatchPrep)
+#     fleet = Fleet([survey_vessel(spec, prep),
+#                    comm_vessel(spec, prep)],
+#                   [length(survey_targeting(spec)), 40_000],
+#                   [1, 2])
 
 
-    SpatQSimSetup(init_pop(prep),
-                  fleet,
-                  movement(prep),
-                  pop_dynamics(spec),
-                  domain(prep),
-                  15,
-                  spec)
-end
+#     SpatQSimSetup(init_pop(prep),
+#                   fleet,
+#                   movement(prep),
+#                   pop_dynamics(spec),
+#                   domain(prep),
+#                   15,
+#                   spec)
+# end
